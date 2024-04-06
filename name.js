914b6fc4055ece76edd522d86e0cc86ce7e319ce
salss33 <hallosalsa03@gmail.com>
2024-04-07 01:39:02 +0700

    First

---
+++ name.js
@@ -0,0 +1,10 @@
+Video.addEventListener("click", function video(){
+    alert("ini video Red Velvet");
+});
+
+Video.addEventListener("click", function audio(){
+    alert("ini Audio Red Velvet");
+});
+Video.addEventListener("click", function gambar(){
+    alert("ini Gambar Red Velvet");
+});
